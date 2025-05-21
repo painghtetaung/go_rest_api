3cda8e59351bff581605b17274a9eb000b736452
kopaing-nuos <kopaing@globalmagicko.com>
2025-05-21 10:19:12 +0700

    home page endpoint setup

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Received %s request for %s", r.Method, r.URL.Path)
+	fmt.Fprintf(w, "Home Page endpoint hit")
+}
+
+func handleRequests() {
+	http.HandleFunc("/", homePage)
+	log.Fatal(http.ListenAndServe(":8081", nil))
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	handleRequests()
 }
