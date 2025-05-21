99332cbfccad07e1583ace493c3ceef641032855
kopaing-nuos <kopaing@globalmagicko.com>
2025-05-21 11:59:51 +0700

    articles post api setup

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/painghtetaung/go_rest_api
 
 go 1.24.2
+
+require github.com/gorilla/mux v1.8.1 // indirect
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"github.com/gorilla/mux"
 )
 
 type Article struct {
@@ -24,15 +25,23 @@ func allArticles(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(articles)
 }
 
+func testPostArticle(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Test POST endpoint hit")
+}
+
 func homePage(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Received %s request for %s", r.Method, r.URL.Path)
 	fmt.Fprintf(w, "Home Page endpoint hit")
 }
 
 func handleRequests() {
-	http.HandleFunc("/", homePage)
-	http.HandleFunc("/articles", allArticles)
-	log.Fatal(http.ListenAndServe(":8081", nil))
+
+	myRouter := mux.NewRouter().StrictSlash(true)
+
+	myRouter.HandleFunc("/", homePage)
+	myRouter.HandleFunc("/articles", allArticles).Methods("GET")
+	myRouter.HandleFunc("/articles", testPostArticle).Methods("POST")
+	log.Fatal(http.ListenAndServe(":8081", myRouter))
 }
 
 func main() {
