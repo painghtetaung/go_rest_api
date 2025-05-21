ffbdade1d1f4c99d952348aed3e1a059ed6b0203
kopaing-nuos <kopaing@globalmagicko.com>
2025-05-21 10:30:29 +0700

    articles endpoint setup

--- main.go
+++ main.go
@@ -1,10 +1,28 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
+
+type Article struct {
+	Title   string `json:"title"`
+	Desc    string `json:"desc"`
+	Content string `json:"content"`
+}
+
+func allArticles(w http.ResponseWriter, r *http.Request) {
+	articles := Article{
+		Title:   "Test Title",
+		Desc:    "Test Description",
+		Content: "Test Content",
+	}
+	// fmt.Fprintf(w, "Endpoint hit: All articles")
+	fmt.Println("Endpoint hit: All articles")
+	json.NewEncoder(w).Encode(articles)
+}
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Received %s request for %s", r.Method, r.URL.Path)
@@ -13,6 +31,7 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 
 func handleRequests() {
 	http.HandleFunc("/", homePage)
+	http.HandleFunc("/articles", allArticles)
 	log.Fatal(http.ListenAndServe(":8081", nil))
 }
 
